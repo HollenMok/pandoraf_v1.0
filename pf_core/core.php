712e81a34b7e57acfd541c1b1f3eb3a1d1528ddb
HollenMok <461836320@qq.com>
2015-07-06 07:38:42 +0800

    add application directory/完善程序文件目录
    1.applications: different application eg. pandoraf=www.pandoraf.com，
    include components, controllers, public models etc.
    /应该程序目录，一般一个域名对应一个程序目录，包括不同组件，控制器，公用模块等
    2.docs:documents for framework
    /框架文档目录
    3.pf_core: core file of framework
    /框架核心文件目录

---
+++ pf_core/core.php
@@ -0,0 +1,26 @@
+<?php
+//-+---------------------------------------------------------------------------------------------+
+//   A Simple and Innovative PHP Framework about Foreign Trade E-commerce @2015-07-01 Version 1.0
+//   一个简单和创新的PHP框架，为外贸电子商务开发， 始于中国共产党建党日,7月1日，版本1.0
+//-+---------------------------------------------------------------------------------------------+
+//   Update from/更新地址@https://github.com/HollenMok/pandoraf_v1.0
+//-+---------------------------------------------------------------------------------------------+
+//   Display on/项目效果展示地址 @http://www.pandoraf.com
+//-+---------------------------------------------------------------------------------------------+
+//   Apache License/开源许可协议 @http://www.apache.org/licenses/LICENSE-2.0
+//-+---------------------------------------------------------------------------------------------+
+//   Document support multi-language, aim to invite people worldwide join this project
+//   文档目标是支持多语言，让全世界的人有机会了解并参加设计这个项目，目前只支持中文与英语。
+//-+---------------------------------------------------------------------------------------------+
+
+//eg. url request=www.pandoraf.com/index.php?com=account&t=login&email=461836320@qq.com&pwd=pf1688
+$controller = $_GET['com'];
+$task = $_GET['t'];
+
+require './pandoraf/execute.php';
+$execute = new execute();
+$execute->controller = $controller;
+$execute->task = $task; 
+$execute->display();
+exit; 
+?>
