5dfd2abe9ccc6b5a2515c63d5a6637f8557d0cb3
HollenMok <461836320@qq.com>
2015-07-18 21:18:28 +0800

    config Linux server for pandoraf/为pandoraf配置Linux服务器
    use aliyun's Linux server and document，install php, mysql, apache etc.
    web environment by myself/使用阿里云的Linux服务器及文档，手动安装apache, mysql,php等web环境

---
+++ templates_c/0157e526d7f3cdce559ae69f776e17f5efd22ec8_0.file.pf.html.php
@@ -0,0 +1,34 @@
+<?php /* Smarty version 3.1.27, created on 2015-07-12 19:43:12
+         compiled from "D:\wamp\www\pandoraPHP\pandoraf_v1.0\applications\pandoraf\templates\view\display\pf.html" */ ?>
+<?php
+/*%%SmartyHeaderCode:1646555a2c35022f662_17292511%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0157e526d7f3cdce559ae69f776e17f5efd22ec8' => 
+    array (
+      0 => 'D:\\wamp\\www\\pandoraPHP\\pandoraf_v1.0\\applications\\pandoraf\\templates\\view\\display\\pf.html',
+      1 => 1436730181,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1646555a2c35022f662_17292511',
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_55a2c350232470_15864356',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_55a2c350232470_15864356')) {
+function content_55a2c350232470_15864356 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '1646555a2c35022f662_17292511';
+?>
+
+<style type="text/css">
+.color{ font-size:16px; color:#666666; line-height:2.4;}
+</style>
+
+<?php }
+}
+?>
---
+++ templates_c/88564f2e193570bfaf614ed1a5d4a1d5f2a64e70_0.file.style.css.php
@@ -0,0 +1,30 @@
+<?php /* Smarty version 3.1.27, created on 2015-07-12 19:47:19
+         compiled from "D:\wamp\www\pandoraPHP\pandoraf_v1.0\applications\pandoraf\templates\view\css\style.css" */ ?>
+<?php
+/*%%SmartyHeaderCode:1286655a2c4470273d4_55636709%%*/
+if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '88564f2e193570bfaf614ed1a5d4a1d5f2a64e70' => 
+    array (
+      0 => 'D:\\wamp\\www\\pandoraPHP\\pandoraf_v1.0\\applications\\pandoraf\\templates\\view\\css\\style.css',
+      1 => 1436729837,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '1286655a2c4470273d4_55636709',
+  'has_nocache_code' => false,
+  'version' => '3.1.27',
+  'unifunc' => 'content_55a2c44702a3b2_47108393',
+),false);
+/*/%%SmartyHeaderCode%%*/
+if ($_valid && !is_callable('content_55a2c44702a3b2_47108393')) {
+function content_55a2c44702a3b2_47108393 ($_smarty_tpl) {
+
+$_smarty_tpl->properties['nocache_hash'] = '1286655a2c4470273d4_55636709';
+?>
+/* pandoraf.com @2015_v1.0*/
+.color{ font-size:16px; color:#666666; line-height:2.4;}<?php }
+}
+?>
