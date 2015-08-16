a1cf0ee8de9e3511884e3117f5557641bea078cb
HollenMok <461836320@qq.com>
2015-08-16 20:32:16 +0800

    open-close principle/开闭原则
    it's the top design principle go through the whole framework
    desgin/这是贯穿整个框架设计的首要设计原则

---
+++ docs/designMode/designPrinciple.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * 
+ * @desc desgin principle/设计原则
+ * 
+ * open-close principle/开闭原则
+ * 
+ * open for extension close for code modification/对扩展开放，对代码修改封闭
+ * 
+ * abstract clas and interface is most important/抽象类与接口是最重要的
+ * 
+ * pakage variable factor is the key/封装可变因素是关键
+ * 
+ * 
+ */
