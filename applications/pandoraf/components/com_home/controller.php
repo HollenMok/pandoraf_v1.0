376e3c9f60ad1ebaf306a65c04e3affeae1616bd
HollenMok <461836320@qq.com>
2015-10-03 16:24:26 +0800

    banner of home page/首页广告
    none finish: banner image url should get from database/还没完成：广告图片链接取自数据库

---
+++ applications/pandoraf/components/com_home/controller.php
@@ -0,0 +1,29 @@
+<?php
+//-+---------------------------------------------------------------------------------------------+
+//   A Simple and Innovative PHP Framework about Foreign Trade E-commerce @2015-07-01 Version 1.0
+//   一个简单和创新的PHP框架，为外贸电子商务开发， 始于中国共产党建党日,7月1日，版本1.0
+//-+---------------------------------------------------------------------------------------------+
+//   Update from/更新地址@https://github.com/HollenMok/pandoraf_v1.0
+//-+---------------------------------------------------------------------------------------------+
+//   Display on/项目效果展示地址 @http://www.pandoraf.com
+//-+---------------------------------------------------------------------------------------------+
+//   Apache License/开源许可协议 @http://www.apache.org/licenses/LICENSE-2.0
+//-+---------------------------------------------------------------------------------------------+
+//   Document support multi-language, aim to invite people worldwide join this project
+//   文档目标是支持多语言，让全世界的人有机会了解并参加设计这个项目，目前只支持中文与英语。
+//-+---------------------------------------------------------------------------------------------+
+class HomeController{
+	public $homeModuleHome;
+	public $smarty;
+	public function __construct(){
+		require 'modules/home.php';
+		$this->homeModuleHome =  new homeModuleHome();
+		//Smarty engine/Smarty 引擎
+		require ROOT.'/pf_core/smarty/libs/Smarty.class.php';
+		$this->smarty = new Smarty();
+	}
+	public function display(){
+		$this->homeModuleHome->display();
+		$this->smarty->display('web/display/home.html');
+	}
+}
---
+++ applications/pandoraf/components/com_home/modules/home.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * -----------------------------
+* module of home component
+* 首页组件模块
+* -----------------------------
+* @author HollenMok
+* @date 2015/10/03
+*
+*/
+class homeModuleHome{
+	public function __construct(){
+
+	}
+	
+	public function display(){
+      return null; 
+	}
+}
