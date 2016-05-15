be6d85b5c751719efd3664d66714174ad37fc601
HollenMok <461836320@qq.com>
2016-05-15 21:23:21 +0800

    product attributes about  size and color/产品属性颜色与尺寸

--- applications/pandoraf/components/com_product/controller.php
+++ applications/pandoraf/components/com_product/controller.php
@@ -23,7 +23,23 @@ class ProductController{
 		$productInfo = $this->productModuleProduct->display();
 		require ROOT.'/applications/pandoraf/models/mod_init/initConfig.php';
 		$this->smarty->assign('Navs',$initConfig);
+		//根据products_id查products_attributs表
+		$attributes['379']['name'] = "color";
+		$attributes['379']['values']['16224']['value_name'] = "green";
+		$attributes['379']['values']['16224']['smallImage'] ="/applications/pandoraf/templates/imgServer/p1050328/small/p1050328(1).jpg";
+		$attributes['379']['values']['16239']['value_name'] = "purple";
+		$attributes['379']['values']['16239']['smallImage'] ="/applications/pandoraf/templates/imgServer/p1050328/small/p1050328(2).jpg";
+		$attributes['379']['values']['16223']['value_name'] = "red";
+		$attributes['379']['values']['16223']['smallImage'] ="/applications/pandoraf/templates/imgServer/p1050328/small/p1050328(3).jpg";
+		$attributes['380']['name'] = "size";
+		$attributes['380']['values']['16218']['value_name'] = "M";
+		$attributes['380']['values']['16234']['value_name'] = "L";
+	    $attributes['380']['values']['16233']['value_name'] = "XL";
+	    $attributes['380']['values']['16235']['value_name'] = "2XL";
+
+		
 		$this->smarty->assign('productInfo',$productInfo);
+		$this->smarty->assign('attributes',$attributes);
 		$this->smarty->display('web/display/product_view.html');exit;
 	}
 
